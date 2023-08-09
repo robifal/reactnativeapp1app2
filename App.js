a4fe081e9d9bfcd698c8ad85003cb4403f9e1b62
Robert <127435909+robifal@users.noreply.github.com>
2023-08-09 15:56:18 -0300

    Foi adicionado o APP 1 e o APP2

--- App.js
+++ App.js
@@ -1,19 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import React, {useState} from 'react';
+// import {Button, Text, View, StyleSheet} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+// const App1 = () => {
+//   const [cliques, setCliques] = useState(0);
+
+//   return (
+//     <View style={styles.container}>
+//     <Button title="OK" onPress={() => {
+//       setCliques(cliques + 1);
+//     }} />
+//     <Text>Quantidade de cliques: {cliques}</Text>
+//     <StatusBar style="auto"/>
+//     </View>
+//   );
+// }
+
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {Text, StyleSheet, View} from "react-native";
+
+
+const Campus = (props) => {
+  return(
+    <Text>O campus {props.nome} foi fundado em {props.anoFundacao}.</Text>
+  )
+}
+
+const InstitutoFederal = (props) => {
+  return(
+    <Text>{props.sigla} - {props.uf}</Text>
   );
 }
+
+const App2 = () => {
+  return(
+    <View style={style.container}>
+      <InstitutoFederal sigla="IFAL" uf="AL" />
+      <Campus nome="Maceió" anoFundacao={1909} />
+      <Campus nome="Rio Largo" anoFundacao={2014} />
+      <Campus nome="Satuba" anoFundacao={1905} />
+      <InstitutoFederal sigla="IFPE" uf="PE" />
+      <Campus nome="Palmares" anoFundacao={2014} />
+      <Campus nome="Garanhuns" anoFundacao={2010} />
+      <StatusBar style="auto"/>
+    </View>
+  )
+}
+
+export default App2;
+// export default App1;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#B0C4DE',
     alignItems: 'center',
     justifyContent: 'center',
   },
